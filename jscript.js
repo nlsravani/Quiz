0f587dfe3017db92fe9720cd17cc1d65b82f4daa
sravani <sravani.nuthi2909@gmail.com>
2020-10-11 13:52:35 +0530

    Initial commit

---
+++ jscript.js
@@ -0,0 +1,32 @@
+
+//Array of objects
+const quiz = [
+     	  {
+     	  	q:'Which month comes right before june ?',
+     	  	options:['may', 'september','july','agust'],
+     	    answer:0
+     	},
+     	{
+     		q: 'what color is banana?',
+     		options:['red','yellow','white','blue'],
+     		answer:1
+
+     	},
+     	{
+     		q: '3 + 4 = 7?',
+     		options: ['true','false'],
+     		answer:0
+     	},
+     	{
+     		q: 'Which have vitmin c ?',
+     		options: ['carrot','grape','lemon','milk'],
+     		answer: 2
+     	},
+     	{
+     		q: 'what is 2 + 6 ?',
+     		options: [3, 12,24,8],
+     		answer: 3
+     	}
+
+
+     	]
